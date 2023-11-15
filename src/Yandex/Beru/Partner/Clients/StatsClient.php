081f972b38f86be33d91fa91cba8f2f5c9ca7bfa
Евгений Гришагин <grishagin@retailcrm.ru>
2023-11-15 19:56:08 +0300

    Add report of order stats

---
+++ src/Yandex/Beru/Partner/Clients/StatsClient.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Yandex\Beru\Partner\Clients;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Yandex\Beru\Partner\Exception\ExtendedErrorsException;
+use Yandex\Beru\Partner\Exception\PartnerRequestException;
+use Yandex\Beru\Partner\Models\Response\GetOrdersStats;
+use Yandex\Beru\Partner\Models\Response\GetStatsResultOrders;
+use Yandex\Beru\Partner\Models\Stats\OrdersStats;
+use Yandex\Common\Exception\ForbiddenException;
+use Yandex\Common\Exception\UnauthorizedException;
+
+class StatsClient extends Client
+{
+    /**
+     * Get report of orders stat by filter
+     *
+     * @param $campaignId
+     * @param array $filter
+     *
+     * @throws ForbiddenException
+     * @throws ExtendedErrorsException
+     * @throws PartnerRequestException
+     * @throws GuzzleException
+     * @throws UnauthorizedException
+     */
+    public function getOrderStats($campaignId, array $filter = [])
+    {
+        $resource = 'campaigns/' . $campaignId . '/stats/orders';
+
+        $response = $this->sendRequest(
+            'POST',
+            $this->getServiceUrl($resource),
+            ['json' => $filter]
+        );
+        $decodedResponseBody = $this->getDecodedBody($response->getBody());
+
+        return new GetOrdersStats($decodedResponseBody);
+    }
+}
